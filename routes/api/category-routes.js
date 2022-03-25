c5c43dd7d522c34a6ac1d6e9903bbdb39ab29f00
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-03-25 19:43:38 +0000

    GET category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,18 +2,31 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', async (req, res) => {
-  try {}
-  catch{}
+  try {
+    const categoryInfo = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
   // find all categories
   // be sure to include its associated Products
-});
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+  router.get("/:id", async (req, res) => {
+    try {
+     //obtains only a single entry from the table, using the provided primary key.
+      const categoryInfo = await Category.findByPk(req.params.id, {
+        include: [{ model: Product }],
+      });
+      res.status(200).json(categoryInfo);
+
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.post('/', (req, res) => {
   // create a new category
